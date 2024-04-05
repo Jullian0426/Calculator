18cd4df0b86d3efffb939879fdc5800bea99fe01
Jullian0426 <jullian0426@yahoo.com>
2024-04-05 03:51:47 -0500

    Create script with operator with basic math functions.

---
+++ script.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if(b === 0) {
+    return "Can't divide by zero!";
+  }
+  return a / b;
+}
+
+let firstNumber = 0;
+let operator = '';
+let secondNumber = 0;
+
+function operate(operator, num1, num2) {
+  switch(operator) {
+    case '+':
+      return add(num1, num2);
+    case '-':
+      return subtract(num1, num2);
+    case '*':
+      return multiply(num1, num2);
+    case '/':
+      return divide(num1, num2);
+    default:
+      return 'Invalid operator';
+  }
+}
